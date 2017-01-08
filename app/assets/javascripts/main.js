a1993287346f40b321c80d9220f980656c024518
Roma Babij <babijua@gmail.com>
2017-01-08 02:14:24 +0200

    clear js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,28 +1,3 @@
-( function( $ ) {
-    $( document ).ready( function() {
-
-        $('#user_user-select').selectize({
-            create: true,
-            sortField: 'text'
-        });
-
-        alert(1);
-
-        $("a").click(function() {
-            alert(2);
-            return false;
-        });
-
-
-        $("body").on('click', 'a', function() {
-            console.log(1111111111);
-            return false;
-        });
-
-    });
-
-})(jQuery);
-
 
 
 
